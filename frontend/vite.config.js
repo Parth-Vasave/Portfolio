7aef8dbd1006c0452421de179da94575582526cd
parth <mailparthvasave@gmail.com>
2025-08-14 12:12:44 +0530

    Fix GitHub Pages blank page issues

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -27,6 +27,6 @@ export default defineConfig({
       },
     },
   },
-  base: process.env.NODE_ENV === 'production' ? '/Portfolio/' : '/',
+  base: './',
   publicDir: 'public', // Explicitly set the public directory
 })
